0ae2376a148d31ad28335db90c99eb55406b2741
SaloniSsSaini <saloniskr@gmail.com>
2026-04-26 22:11:30 +0530

    Update API URL for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ app.get("/", (req, res) => {
 
 app.use("/api", metricsRoutes);
 
-const PORT = 5000;
+// ✅ FIX HERE
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
-
+const BASE_URL = "https://devinsight-ab9t.onrender.com/api";
 export const fetchData = async (developerId) => {
   const res = await axios.get(`${BASE_URL}/metrics/${developerId}`);
   return res.data;
